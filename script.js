daadc875d57fbd1f2efa847301b92139da8b8b70
bas11111 <brouwer.bas@gmail.com>
2022-03-23 17:31:06 +0100

    d

---
+++ script.js
@@ -0,0 +1,8 @@
+let player = document.getElementById('#player');
+let block = document.getElementById('#block');
+
+// Poppetje moet springen als er geklikt word met de muis //
+// Animate moet worden opgeroepen met muis klik //
+    function jump(){
+        console.log('hi')
+}
